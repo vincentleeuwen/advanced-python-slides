14ac5cd92a702a4dff59b0d5540adae02297607b
Remco Wendt <remco.wendt@gmail.com>
2017-04-14 11:06:36 +0200

    Don't use any slide animations

--- conf.py
+++ conf.py
@@ -90,7 +90,10 @@ html_use_index = False
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    "transition": "none",
+}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
